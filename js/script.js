2916108f2aeb415b926d86d76d2980ffaa4a290c
MareikeFla <mwidderich@gmail.com>
2023-11-04 21:07:35 +0100

    Javascript on dots for color changing, transition color change

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const buttonRed = document.querySelector(".highlightdot.red");
+const buttonBlue = document.querySelector(".highlightdot.blue");
+const buttonOrange = document.querySelector(".highlightdot.orange");
+const buttonGreen = document.querySelector(".highlightdot.green");
+const buttonPurple = document.querySelector(".highlightdot.purple");
+let currentButton = buttonBlue;
+
+function setupEventListener(button, color) {
+  button.addEventListener("click", () => {
+    document.documentElement.style.setProperty("--highlightColor", color);
+    button.classList.add("activeHighlightdot");
+    currentButton.classList.remove("activeHighlightdot");
+    currentButton = button;
+  });
+}
+
+setupEventListener(buttonRed, "var(--highlightRed)");
+setupEventListener(buttonBlue, "var(--highlightBlue)");
+setupEventListener(buttonOrange, "var(--highlightOrange)");
+setupEventListener(buttonGreen, "var(--highlightGreen)");
+setupEventListener(buttonPurple, "var(--highlightPurple)");
